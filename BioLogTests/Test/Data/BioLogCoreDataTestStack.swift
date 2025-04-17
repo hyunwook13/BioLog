670cbfb4f02678f604a72e1a0a129b61161da7ad
Hyunwook <hjhsha827@gmail.com>
2025-04-17 15:02:26 +0900

    add: CoreData Unit test

---
+++ BioLogTests/Test/Data/BioLogCoreDataTestStack.swift
@@ -0,0 +1,39 @@
+//
+//  BioLogCoreDataTestStack.swift
+//  BioLogTests
+//
+//  Created by 이현욱 on 4/3/25.
+//
+
+import Foundation
+import CoreData
+
+// https://www.youtube.com/watch?v=DTz_MFxe9mk&ab_channel=SwiftArcade
+
+class BioLogCoreDataTestStack {
+    
+    let persistentContainer: NSPersistentContainer
+    let backgroundContext: NSManagedObjectContext
+    let mainContext: NSManagedObjectContext
+    
+    init() {
+        persistentContainer = NSPersistentContainer(name: "BioLog")
+        let description = persistentContainer.persistentStoreDescriptions.first
+        description?.type = NSInMemoryStoreType
+        
+        persistentContainer.loadPersistentStores { description, error in
+            guard error == nil else {
+                fatalError("was unable to load store \(error!)")
+            }
+        }
+        
+        mainContext = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+        mainContext.automaticallyMergesChangesFromParent = true
+        mainContext.persistentStoreCoordinator = persistentContainer.persistentStoreCoordinator
+
+        backgroundContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        backgroundContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        backgroundContext.parent = self.mainContext
+    }
+}
+
---
+++ BioLogTests/Test/Data/CoreDataTests.swift
@@ -0,0 +1,64 @@
+//
+//  CoreDataTests.swift
+//  BioLogTests
+//
+//  Created by 이현욱 on 4/3/25.
+//
+
+import Foundation
+import XCTest
+
+@testable import BioLog
+
+class CoreDataTests: XCTestCase {
+    
+    var testCoredataStack: BioLogCoreDataTestStack!
+    var coreData: CoreData!
+    
+    override func setUp() {
+        self.testCoredataStack = BioLogCoreDataTestStack()
+        self.coreData = CoreData(mainContext: testCoredataStack.mainContext, container: nil)
+    }
+    
+//    override func tearDown() {
+//        testCoredataStack = nil
+//        coreData = nil
+//    }
+    
+    func test_코아데이터_생성_테스트() {
+        
+        // given
+        let sampleBooks = BookDummy.sampleBooks
+        
+        // when 
+        for book in sampleBooks {
+            let createdBook = coreData.create(book)
+            
+            // then
+            XCTAssertNotNil(createdBook)
+            
+            // Observable로 감싸진 Book 객체 테스트
+            _ = createdBook.subscribe(onNext: { bookObject in
+                XCTAssertEqual(bookObject.title, book.title)
+                XCTAssertEqual(bookObject.author, book.author)
+                XCTAssertEqual(bookObject.book_description, book.description)
+                XCTAssertEqual(bookObject.isbn, book.isbn)
+                XCTAssertEqual(bookObject.cover, book.cover)
+                XCTAssertEqual(bookObject.pubDate, book.pubDate)
+                XCTAssertEqual(bookObject.categoryName, book.categoryName)
+            })
+        }
+        
+
+    }
+    
+    func test_데이터생성후_데이터조회() {
+        // when
+        let fetchedBooks = coreData.fetch(Book.self)
+        
+        _ = fetchedBooks.subscribe(onNext: { bookObject in
+            // then
+            XCTAssertEqual(bookObject.count, 5)
+        })
+    }
+}
