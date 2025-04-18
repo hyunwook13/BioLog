2e575c5d7fbdcc9c7afc73d956c760b5c3cc2ca0
Hyunwook <hjhsha827@gmail.com>
2025-04-18 23:20:28 +0900

    feat: Fix Entity with mapping model

---
+++ BioLog/Source/Data/PersistentStorages/BookMigrationPolicy.swift
@@ -0,0 +1,28 @@
+//
+//  BookMigrationPolicy.swift
+//  BioLog
+//
+//  Created by 이현욱 on 4/7/25.
+//
+
+import Foundation
+import CoreData
+
+final class BookMigrationPolicy: NSEntityMigrationPolicy {
+    override func createDestinationInstances(
+        forSource sourceInstance: NSManagedObject,
+        in mapping: NSEntityMapping,
+        manager: NSMigrationManager
+    ) throws {
+        try super.createDestinationInstances(forSource: sourceInstance, in: mapping, manager: manager)
+        
+        guard let book = manager.destinationInstances(forEntityMappingName: mapping.name, sourceInstances: [sourceInstance]).first else {
+            return
+        }
+
+        if let bookmarked = sourceInstance.value(forKey: "bookmark") as? NSManagedObject {
+            let isAdded = bookmarked.value(forKey: "addedDate") as? Date ?? Date()
+            book.setValue(isAdded, forKey: "addedDate")
+        }
+    }
+}
